1e0eb414bcbd15ce52b9f30cf58ac1c482dae6aa
DinoLai <dinos80152@gmail.com>
2017-07-30 16:49:57 +0800

    get port from env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +12,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	port := "8080"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	http.ListenAndServe(":"+port, nil)
 }
